e1beaac47af835a4287cc344ac5d786331ba7e58
AndroUn <56448483+AndroUn@users.noreply.github.com>
2019-11-05 22:25:06 +0400

    responsive

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+function openReg() {
+  document.getElementById("myReg").style.display = "block";
+  document.getElementById("myForm").style.display = "none";
+}
+function closeReg() {
+  document.getElementById("myReg").style.display = "none";
+}
